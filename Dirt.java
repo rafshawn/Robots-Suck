b2225ba0238ae126097e2a91f62425254191cb5d
rafshawn <rafshawn@yandex.com>
2023-06-09 15:22:30 -0230

    Room Class Update

--- Dirt.java
+++ Dirt.java
@@ -2,6 +2,30 @@
  * Dirt
  */
 public class Dirt {
+  private int x;
+  private int y;
+  private boolean cleaned;
 
+  public Dirt(int x, int y) {
+    this.x = x;
+    this.y = y;
+    cleaned = false;
+  }
+
+  public int getX() {
+    return x;
+  }
+
+  public int getY() {
+    return y;
+  }
+
+  public boolean isCleaned() {
+    return cleaned;
+  }
+
+  public void clean() {
+    cleaned = true;
+  }
   
 }
--- Room.java
+++ Room.java
@@ -1,50 +1,76 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * Room
  */
 
 public class Room {
-  private int[][] grid;   // number of grids (horizontal, vertical)
-  private int size;       // room size
-  private int[][] dirts;  // dirts (horizontal, vertical)
+  private int size;
+  private int[][] cell;
+  private ArrayList<Dirt> dirts;
 
   // initialise room inside constructor
   public Room(int size) {
     this.size = size;
-    grid = new int[size][size];
+    cell = new int[size][size];
     initializeRoom();
   }
 
   private void initializeRoom() {
-    // TODO: Initialize the room
-    for (int i = 0; i < size; i++) {
-      for (int j = 0; j < size; j++) {
-        grid[i][j] = 0;
+    for (int i = 0; i < size; i++) {      // iterate through rows
+      for (int j = 0; j < size; j++) {    // iterate through columns
+        cell[i][j] = 0;                   // sets current cell to 0 (dirty)
       } 
     }
-
   }
 
-  public int[][] getGrid(){
-    return grid;
+  private static int getRoomSize(String filename) throws FileNotFoundException {
+    // takes the size of the room from the file `room.txt`
+    filename = "./Text-Files/room.txt";
+    File file = new File(filename);
+    Scanner sc = new Scanner(file);
+    int i = sc.nextInt();
+    sc.close();
+
+    System.out.println("Input File: " + filename);
+    System.out.println("Room size: " + i + "x" + i);
+
+    return i;
   }
 
   private void generateDirt() {
-    // TODO: Generate dirt
-    
+    // Logic to generate dirt:
+    // We know we have the room size, we want to randomly generate dirts
+    // within the constraints of the room and never occupying spaces a robot has occupied.
+    // i.e., no. of dirt < no. of available cells - no. of robot occupied cells.
+    // Since there has to be at least one robot, we can use a while loop to generate dirts < no. of available cells.
+
+    dirts = new ArrayList<Dirt>();
+
+    Random rand = new Random();
+    int maxOccupiable = (size * size) - 1;
+    int maxDirts = rand.nextInt(maxOccupiable) + 1;
+
+    while (dirts.size() < maxDirts) {
+      int x = (int) (Math.random() * size);
+      int y = (int) (Math.random() * size);
+      if (cell[x][y] == 0) {
+        dirts.add(new Dirt(x, y));
+      }
+    }
+
+    System.out.println("Dirt generated: " + dirts.size());
   }
 
-  public int[][] getDirtArrays() {
-    // TODO: Generate dirt
+  public int[][] getCell(){
+    return cell;
+  }
+
+  public ArrayList<Dirt> getDirtArrays() {
     return dirts;
   }
-
-  private void readRoomFile(){
-    // takes the size of the room from the file `room.txt`
-  }
-
-  // TODO: Add necessary methods
-
-  // TODO: Implement additional functionality
-
-  // TODO: Add any other required methods
 }
